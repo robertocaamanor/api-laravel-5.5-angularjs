dc963dc4ce61da82b703e32e247f1ebbeb212e8c
Roberto <raristides.caamano@gmail.com>
2017-12-26 18:53:48 -0300

    Añadido API Tipo Productos

---
+++ app/TipoProducto.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class TipoProducto extends Model
+{
+    //
+
+    protected $table = 'tipoproducto';
+
+    protected $fillable = 'nombre';
+}
--- routes/api.php
+++ routes/api.php
@@ -18,3 +18,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::resource('producto', 'ProductoController');
+Route::resource('tipo-producto', 'TipoProductoController');
